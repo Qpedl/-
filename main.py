2a3e0fb810438d3f72ef91e6c65eaa36e7e6a4df
Qpedl <111699956+Qpedl@users.noreply.github.com>
2022-08-22 15:10:12 +0800

    Update main.py

--- main.py
+++ main.py
@@ -64,4 +64,5 @@ wm = WeChatMessage(client)
 wea, temperature = get_weather()
 data = {"weather":{"value":wea},"temperature":{"value":temperature},"love_days":{"value":get_count()},"birthday2_left":{"value":get_birthday2()},"birthday1_left":{"value":get_birthday1()},"birthday_left":{"value":get_birthday()},"words":{"value":get_words(), "color":get_random_color()}}
 res = wm.send_template(user_id, template_id, data)
+res = wm.send_template(user1_id, template_id, data)
 print(res)
