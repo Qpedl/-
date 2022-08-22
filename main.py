20df3dc8bd87d888d2740b93b1563d2a1db8e246
Qpedl <111699956+Qpedl@users.noreply.github.com>
2022-08-22 12:38:56 +0800

    Update main.py

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ today = datetime.now()
 start_date = os.environ['START_DATE']
 city = os.environ['CITY']
 birthday = os.environ['BIRTHDAY']
-birthday = os.environ['BIRTHDAY1']
+birthday1 = os.environ['BIRTHDAY1']
 app_id = os.environ["APP_ID"]
 app_secret = os.environ["APP_SECRET"]
 
